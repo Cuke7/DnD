f023b08d922e4f5551d64c574faca8be1483ffd5
Cuke7 <cuke.dev@mail.com>
2021-05-07 14:49:46 +0200

    Is Mélanie ritht ? (she's not)

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: "%s - website",
-    title: "website",
+    title: "D&D app",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
