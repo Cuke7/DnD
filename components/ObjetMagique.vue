cf3e6bd49fa90a3dc832e16f4ccee61cd2a4780c
Cuke7 <cuke.dev@mail.com>
2021-05-07 20:20:37 +0200

    YES

--- components/ObjetMagique.vue
+++ components/ObjetMagique.vue
@@ -1,25 +1,27 @@
 <template>
-  <v-container>
-    <v-card color="rgb(255, 255, 255, 0.4)" class="pa-5">
-      <v-row justify="space-between" align="center">
-        <v-col cols="auto">
-          <v-row class="subtitle-1">
-            {{ objet.nom }}
-          </v-row>
-          <v-row class="subtitle-2 mt-4">
-            {{ objet.type }}, {{ objet.rarete }}.
-          </v-row>
-        </v-col>
-        <v-col cols="auto" class="ma-0 pa-0">
-          <v-btn nuxt v-bind:to="'/DnD/' + objet.nom" icon>
-            <v-icon>
-              mdi-chevron-right
-            </v-icon>
-          </v-btn>
-        </v-col>
-      </v-row>
-    </v-card>
-  </v-container>
+  <v-lazy>
+    <v-container>
+      <v-card color="rgb(255, 255, 255, 0.4)" class="pa-5">
+        <v-row justify="space-between" align="center">
+          <v-col cols="auto">
+            <v-row class="subtitle-1">
+              {{ objet.nom }}
+            </v-row>
+            <v-row class="subtitle-2 mt-4">
+              {{ objet.type }}, {{ objet.rarete }}.
+            </v-row>
+          </v-col>
+          <v-col cols="auto" class="ma-0 pa-0">
+            <v-btn nuxt v-bind:to="'/DnD/' + objet.nom" icon>
+              <v-icon>
+                mdi-chevron-right
+              </v-icon>
+            </v-btn>
+          </v-col>
+        </v-row>
+      </v-card>
+    </v-container>
+  </v-lazy>
 </template>
 
 <script>
