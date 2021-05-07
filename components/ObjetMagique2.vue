804f9ae491a79305003cee2946fd82ec148792d5
Cuke7 <cuke.dev@mail.com>
2021-05-07 19:01:15 +0200

    yes

---
+++ components/ObjetMagique2.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-lazy>
+    <div>
+      <v-list-item>
+        <v-list-item-avatar>
+          <v-avatar v-if="objet.image_url != 'no_url'">
+            <img :src="objet.image_url" alt="item.nom" />
+          </v-avatar>
+          <v-avatar v-else color="white">
+            {{ objet.nom[0] }}
+          </v-avatar>
+        </v-list-item-avatar>
+
+        <v-list-item-content>
+          <v-list-item-title>{{ objet.nom }}</v-list-item-title>
+        </v-list-item-content>
+
+        <v-list-item-action>
+          <v-btn nuxt v-bind:to="'/DnD/' + objet.nom" icon>
+            <v-icon>
+              mdi-chevron-right
+            </v-icon>
+          </v-btn>
+        </v-list-item-action>
+      </v-list-item>
+      <v-divider class="mt-2"></v-divider>
+    </div>
+  </v-lazy>
+</template>
+
+<script>
+export default {
+  name: "ObjetMagique",
+  props: ["objet"]
+};
+</script>
