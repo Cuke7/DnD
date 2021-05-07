f3d3651a829765d8e8934080ddfddf24577e45f8
Cuke7 <cuke.dev@mail.com>
2021-05-07 21:00:02 +0200

    YES

--- components/ObjetMagique2.vue
+++ components/ObjetMagique2.vue
@@ -1,28 +1,28 @@
 <template>
   <v-lazy>
     <v-container>
-      <v-row align="center" justify="space-between" class="my-1">
-        <v-cols cols="auto">
+      <v-row align="center" justify="space-between">
+        <v-col cols="auto">
           <v-avatar v-if="objet.image_url != 'no_url'">
             <img :src="objet.image_url" alt="item.nom" />
           </v-avatar>
           <v-avatar v-else color="white">
             {{ objet.nom[0] }}
           </v-avatar>
-        </v-cols>
-        <v-cols cols="auto">
+        </v-col>
+        <v-col cols="auto">
           {{ objet.nom }}
-        </v-cols>
-        <v-cols cols="auto">
+        </v-col>
+        <v-col cols="auto">
           <v-btn nuxt v-bind:to="'/DnD/' + objet.nom" icon>
             <v-icon>
               mdi-chevron-right
             </v-icon>
           </v-btn>
-        </v-cols>
+        </v-col>
       </v-row>
       <v-row>
-        <v-divider class ="mt-2"></v-divider>
+        <v-divider class="mt-2"></v-divider>
       </v-row>
     </v-container>
   </v-lazy>
