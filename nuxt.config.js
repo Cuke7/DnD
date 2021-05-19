c3c07fe75335c59b1486daae46c2780ed97c0f40
Cuke7 <cuke.dev@mail.com>
2021-05-19 18:48:41 +0200

    ES

--- nuxt.config.js
+++ nuxt.config.js
@@ -69,5 +69,9 @@ export default {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
 
-  target: "static"
+  target: "static",
+
+  generate: {
+    routes: ["/item/Amulette%20antidote"]
+  }
 };
